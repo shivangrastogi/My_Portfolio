5e22c1ea2587e75c406abd4ede838ffbbea7a56b
Shivang Rastogi <shivangrastogi73@gmail.com>
2025-05-02 20:39:06 +0530

    mobile responsiveness added for 3D

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
-
+import useIsMobile from "./hooks/useIsMobile";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
@@ -35,28 +35,76 @@ const ServiceCard = ({ index, title, icon }) => (
 );
 
 const About = () => {
-  return (
-    <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
-      </motion.div>
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
-        I'm Shivang Rastogi, a passionate full-stack developer skilled in Java,
-        React, Node.js, and SQL. I specialize in building robust web
-        applications and intuitive interfaces. With hands-on experience in
-        project development and problem-solving, I'm eager to collaborate and
-        transform your ideas into reality.
-      </motion.p>
+  const isMobile = useIsMobile();
+
+  return (
+    // <>
+    //   <motion.div variants={textVariant()}>
+    //     <p className={styles.sectionSubText}>Introduction</p>
+    //     <h2 className={styles.sectionHeadText}>Overview.</h2>
+    //   </motion.div>
+
+    //   <motion.p
+    //     variants={fadeIn("", "", 0.1, 1)}
+    //     className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+    //   >
+    //     I'm Shivang Rastogi, a passionate full-stack developer skilled in Java,
+    //     React, Node.js, and SQL. I specialize in building robust web
+    //     applications and intuitive interfaces. With hands-on experience in
+    //     project development and problem-solving, I'm eager to collaborate and
+    //     transform your ideas into reality.
+    //   </motion.p>
+
+    //   <div className="mt-20 flex flex-wrap gap-10">
+    //     {services.map((service, index) => (
+    //       <ServiceCard key={service.title} index={index} {...service} />
+    //     ))}
+    //   </div>
+    // </>
+    <>
+      {isMobile ? (
+        <div>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </div>
+      ) : (
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </motion.div>
+      )}
+
+      {isMobile ? (
+        <p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+          I'm Shivang Rastogi...
+        </p>
+      ) : (
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          I'm Shivang Rastogi...
+        </motion.p>
+      )}
 
       <div className="mt-20 flex flex-wrap gap-10">
-        {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
-        ))}
+        {services.map((service, index) =>
+          isMobile ? (
+            <div className="xs:w-[250px] w-full" key={index}>
+              <div className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+                <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+                  <img src={service.icon} className="w-16 h-16 object-contain" />
+                  <h3 className="text-white text-[20px] font-bold text-center">
+                    {service.title}
+                  </h3>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <ServiceCard key={service.title} index={index} {...service} />
+          )
+        )}
       </div>
     </>
   );
