338e79b6cef1b40631c3650a5057b972846b6400
Shivang Rastogi <shivangrastogi73@gmail.com>
2025-05-02 20:20:09 +0530

    mobile responsiveness added for 3D

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 800px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+        <div className="w-28 h-28" key={technology.name}>
+          {isMobile ? (
+            <img
+              src={technology.icon}
+              alt={technology.name}
+              className="w-full h-full object-contain"
+            />
+          ) : (
+            <BallCanvas icon={technology.icon} />
+          )}
         </div>
       ))}
     </div>
@@ -17,3 +42,25 @@ const Tech = () => {
 };
 
 export default SectionWrapper(Tech, "");
+
+
+
+// import React from "react";
+
+// import { BallCanvas } from "./canvas";
+// import { SectionWrapper } from "../hoc";
+// import { technologies } from "../constants";
+
+// const Tech = () => {
+//   return (
+//     <div className='flex flex-row flex-wrap justify-center gap-10'>
+//       {technologies.map((technology) => (
+//         <div className='w-28 h-28' key={technology.name}>
+//           <BallCanvas icon={technology.icon} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SectionWrapper(Tech, "");
