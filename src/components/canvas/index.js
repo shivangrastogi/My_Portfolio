e10530175760e7e06b27219cf9248e25f1560f2f
Shivang Rastogi <shivangrastogi73@gmail.com>
2025-05-02 18:48:48 +0530

    first commit

--- src/components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import EarthCanvas from "./Earth";
-import { BallCanvas, Ball } from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
-
-export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, Ball };
